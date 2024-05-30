33c96660ffff502a04479635b99557e722038a4a
AlhemyD <138218446+AlhemyD@users.noreply.github.com>
2024-05-31 00:02:50 +0800

    Update models.py

--- models.py
+++ models.py
@@ -24,12 +24,12 @@ class RoleEnum(str, Enum):
 
 #Профиль зарегестрированного пользователя
 class UserProfile(BaseModel):
-    user_id: int = 10
-    photo: str = "not"
-    organization: str = "not"
-    assigned_dogs: List[int] = None
-    last_online: str = "off"
-    tasks: List[int] = None
+    user_id: int
+    photo: str = ""
+    organization: str = ""
+    assigned_dogs: List[int] = []
+    last_online: str = ""
+    tasks: List[int] = []
     role: RoleEnum = "User"
     email: str
     name: str
@@ -38,8 +38,14 @@ class UserProfile(BaseModel):
 class Role(BaseModel):
     role_name: RoleEnum
 
+#Задача
 class Task(BaseModel):
     task_id: int
     description: str
-    verified: bull
+    verified: bool
     executors: List[int]
+
+#Уведомление
+class Notification(BaseModel):
+    reciever: int
+    message: str
